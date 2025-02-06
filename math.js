729ea624bdf313d2541c633a449fcdead3f4907c
Vahide Segah Üney <vahidesegah@gmail.com>
2025-02-06 16:14:35 +0300

    number math

---
+++ math.js
@@ -0,0 +1,10 @@
+console.log(Math.round(5.255)); // en yakına yuvarla
+console.log(Math.ceil(5.255)); // yukarı yuvarla
+console.log(Math.floor(5.255)); // aşağıya yuvarla
+
+console.log(Math.abs(-25.2)); // 25.2
+console.log(Math.min(23,5,1,9)); //1
+console.log(Math.max(23,5,1,9)); // 23
+
+console.log(Math.random());
+console.log(Math.round(Math.random()*100));
---
+++ number.js
@@ -0,0 +1,11 @@
+// var b=7;
+// console.log(a+b);
+// console.log(a.toString()+b.toString()); // yan yana yazar
+
+//var a=5.25478;
+//console.log(a.toFixed(2)); // noktadan sonra 2 karakter 
+
+//console.log(a.toPrecision(2)); // toplam iki karakter, noktadan önce ve sonra
+
+a.parseInt --- // sayyılarla başlayan bir string i number a dönüştürür 
+a.parseFloat --- // string in içinde gördüğü ilk ondaklıklı sayıyı number a çevirir
